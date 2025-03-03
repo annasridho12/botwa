aa0639bb74ec086b196b6cf24249a6328d108336
annasridho12 <140548325+annasridho12@users.noreply.github.com>
2025-03-03 19:27:40 +0700

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.owner = ['6285773305337'] //Ganti
-global.botName = 'Titanz Store' //Ganti
-global.ownerName = 'JF Dev' //Ganti
+global.owner = ['628988033295'] //Ganti
+global.botName = 'Absurbd store' //Ganti
+global.ownerName = 'Nas bre' //Ganti
 global.sessionName = 'session-bot'
 
 global.user = 'xxx' // Daftar di https://j-f.cloud/api/wl
@@ -15,10 +15,10 @@ global.platinum = 100000 //Ganti harga role platinum
 global.pay = `Untuk Pembayaran Silahkan Transfer ke Payment dibawah ini
 
 Bank dan E-Wallet
-7635169958 | BCA
-085773305337 | DANA (+100 perak)
-085773305337 | GOPAY
-085773305337 | OVO
+1140023230835 | MANDIRI
+087796183366 | DANA (+100 perak)
+087796183366 | GOPAY
+087796183366 | OVO
 Pembayaran dari Bank ke E-Wallet Wajib +1500 !!!
 
 Note :
@@ -27,12 +27,12 @@ Berani Kirim Bukti TF Palsu atau Bukti punya Orang lain
 Auto Kick Permanen!!`
 
 // Mongodb
-global.mongodb = "mongodb+srv://" //Ganti url databasemu
-global.database = "botdb" //Ganti sesuai nama databasemu
+global.mongodb = "mongodb+srv://iki69:iki69@cluster0.mrq8e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //Ganti url databasemu
+global.database = "iki69" //Ganti sesuai nama databasemu
 
 // Paydisini Apikey
 global.paydisini_apikey = "APIKEY"; // APIKEY Paydisini.co.id
-global.paydisini_nomer = 085773305337;
+global.paydisini_nomer = 087796183366;
 global.minimalDepoOtomatis = 1000; // Minimal Deposit
 global.maximalDepoOtomatis = 1000000; // Maksimal Deposit
 
